76dea75429b259ca0afcb7400c63b97b9238e9d0
Daniel Suchý <suchydan@gmail.com>
2021-08-27 01:45:17 +0200

    fix: fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,12 @@
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
-    resolve: {
-      plugins: [new TsconfigPathsPlugin()],
-    },
-  });
+exports.onCreateWebpackConfig = ({ actions, stage, plugins }) => {
+  if (stage === 'build-javascript' || stage === 'develop') {
+    actions.setWebpackConfig({
+      plugins: [
+        plugins.provide({ process: 'process/browser' }),
+        new TsconfigPathsPlugin(),
+      ],
+    });
+  }
 };
