8a16d33e852ba4880252a7a3f388d52b774fec6e
Daniel Suchý <suchydan@gmail.com>
2021-08-27 01:32:14 +0200

    feat: github pages publish

---
+++ docs/component---src-pages-404-tsx-e139918b5c9d5e9956e6.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_ts=self.webpackChunkgatsby_starter_ts||[]).push([[218],{938:function(t,e,n){n.r(e);var r=n(7294);e.default=function(){return r.createElement("main",null,r.createElement("p",null,"Sorry, page not found!"))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-e139918b5c9d5e9956e6.js.map
